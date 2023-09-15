4300cdcf3be94a6eb9aacc397b85fa9cfc6138e5
atulsingh369 <businessatul7684@gmail.com>
2023-09-16 03:19:26 +0530

    Updated Setting Page

--- src/Pages/Setting.jsx
+++ src/Pages/Setting.jsx
@@ -3,6 +3,7 @@ import SettingLoader from "../Components/Loaders/SettingLoader";
 import CheckBox from "../Components/Togglers/CheckBox";
 import DataSaver from "../Components/Togglers/DataSaver";
 import AudioCheck from "../Components/Togglers/AuidoCheck";
+import { JackInTheBox } from "react-awesome-reveal";
 
 const Setting = ({ setMiniPlayer }) => {
   const [loading, setLoading] = useState(true); // Set Loading
@@ -21,38 +22,46 @@ const Setting = ({ setMiniPlayer }) => {
       ) : (
         <>
           <div className="lg:p-10 bg-black text-white rounded-box lg:text-2xl border-2 w-screen m-2 lg:w-2/3 border-white space-y-12">
-            {/* Insert a Vector Art here.. */}
-            <p className="text-center text-lg lg:text-2xl lg:mt-0 mt-8 font-bold">
+            <p className="animate__animated animate__bounceIn text-center text-lg lg:text-2xl lg:mt-0 mt-8 font-bold">
               Account Type : Guest
             </p>
+
+            {/* settings */}
             <div className="grid grid-cols-1 lg:grid-cols-2 gap-10 lg:gap-48 w-full my-10">
+              {/* 1st block */}
               <div className="space-y-12">
-                <div className="flex justify-between px-2">
-                  <p>Audio Quality</p>
-                  <AudioCheck />
-                </div>
-                <div className="flex justify-between items-center px-2">
-                  <p>Auto Mix</p>
-                  <CheckBox />
-                </div>
-                <div className="flex justify-between items-center px-2">
-                  <p>Gapless</p>
-                  <CheckBox />
-                </div>
+                <JackInTheBox cascade={true}>
+                  <div className="flex justify-between px-2">
+                    <p>Audio Quality</p>
+                    <AudioCheck />
+                  </div>
+                  <div className="flex justify-between items-center px-2">
+                    <p>Auto Mix</p>
+                    <CheckBox />
+                  </div>
+                  <div className="flex justify-between items-center px-2">
+                    <p>Gapless</p>
+                    <CheckBox />
+                  </div>
+                </JackInTheBox>
               </div>
+
+              {/* 2nd block */}
               <div className="space-y-12">
-                <div className="flex justify-between items-center px-2">
-                  <p>Data Saver</p>
-                  <DataSaver />
-                </div>
-                <div className="flex justify-between items-center px-2">
-                  <p>Normalize Volume</p>
-                  <CheckBox />
-                </div>
-                <div className="flex justify-between items-center p-2">
-                  <p>Crossfade</p>
-                  <CheckBox />
-                </div>
+                <JackInTheBox cascade={true}>
+                  <div className="flex justify-between items-center px-2">
+                    <p>Data Saver</p>
+                    <DataSaver />
+                  </div>
+                  <div className="flex justify-between items-center px-2">
+                    <p>Normalize Volume</p>
+                    <CheckBox />
+                  </div>
+                  <div className="flex justify-between items-center p-2">
+                    <p>Crossfade</p>
+                    <CheckBox />
+                  </div>
+                </JackInTheBox>
               </div>
             </div>
           </div>
